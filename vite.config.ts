7b8306d6bff2cc6878e4bf9a7ad6d79e8056c6fe
ipigtw <diegogamingtw@gmail.com>
2024-05-30 13:33:07 +0800

    e

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "ipigtw.github.io/",
+  base: "/website/",
   plugins: [react()],
 })
